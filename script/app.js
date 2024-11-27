800879a5e4273e13820c4b3ae5d5767da27eb51c
Fahim Shirzad <fahimshirzaduk@gmail.com>
2024-11-27 16:12:01 +0000

    Sorting functionality added along with a button to change the sort direction from ascending to descending

--- script/app.js
+++ script/app.js
@@ -3,12 +3,33 @@ var app = new Vue({
     data: {
         sitename: 'School Lessons & Activities',
         showCart: false,
-        productList: []
+        productList: [],
+        sortKey: 'topic',
+        sortOrder: 1 // 1 for ascending, -1 for descending
     },
     methods: {
         toggleView() {
             this.showCart = !this.showCart;
+        },
+
+        sortBy(key) {
+            if (this.sortKey !== key) {
+                this.sortKey = key; // Change the sort key
+                this.sortOrder = 1; // Default to ascending when a new key is selected
+            }
+            this.applySort();
+        },
+        toggleSortOrder() {
+            this.sortOrder *= -1; // Toggle the sort order
+            this.applySort();
+        },
+        applySort() {
+            this.productList.sort((a, b) => {
+                let modifier = this.sortOrder;
+                return (a[this.sortKey] > b[this.sortKey] ? 1 : a[this.sortKey] < b[this.sortKey] ? -1 : 0) * modifier;
+            });
         }
+
     },
     created() {
         fetch('https://cst3144-cw-server.onrender.com/lessons')
