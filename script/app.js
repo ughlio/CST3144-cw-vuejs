564726245b763ebf0360e35d486f05473cad7077
Fahim Shirzad <fahimshirzaduk@gmail.com>
2024-12-02 06:32:44 +0000

    spelling error

--- script/app.js
+++ script/app.js
@@ -176,7 +176,7 @@ var app = new Vue({
                     location: item.location,
                     price: item.price,
                     space: item.space,
-                    imageUrls: '//cst3144-cw-server.onrender.com/images/${item.imageName}'
+                    imageUrl: '//cst3144-cw-server.onrender.com/images/${item.imageName}'
                 }));
             })
             .catch(error => console.error('Failed to load lessons:', error));
